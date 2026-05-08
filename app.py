cde25a8aaefa650d6f60bb95f1532a5d852718cc
hakonnys <hakon.n@icloud.com>
2026-05-08 10:41:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ app.add_middleware(
 )
 
 def get_db():
-    conn= sqlite3.connect(DB_PATH)
+    conn= sqlite3.connect(DB_PATH, check_same_thread=False)
     conn.row_factory = sqlite3.Row
     return conn
 
